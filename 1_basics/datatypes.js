22e17b39fcecca60ea16fcbe31b07bfaf581c8ed
nishant-dotcom <prajapatinishant2433@gmail.com>
2024-06-17 15:38:13 +0000

    variables & datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use stricts";
+
+console.log(3+3)
+console.log("Nishant")
+
+let name="Nishant"
+let age=21
+let isLoggedIn =false
+
+//number = range is upto 2 to power of 53
+//bigint = for biggest number
+//string = ""
+//boolean = True/False
+//null = standalone value /empty value
+//undefine = where value is not assigned yet
+//symbol = for uniqueness
+
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
---
+++ 1_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=1234
+let accountEmail="ntp@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+accountEmail="trp@gmail.com"
+accountPassword="09876"
+accountCity="surat"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
