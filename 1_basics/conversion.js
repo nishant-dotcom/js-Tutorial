16149ee55f2b6ff08e57237a2a84b03891613d4a
nishant-dotcom <prajapatinishant2433@gmail.com>
2024-06-18 10:21:43 +0000

    conversion of datatypes

---
+++ 1_basics/conversion.js
@@ -0,0 +1,15 @@
+let score="33"
+console.log(typeof score);
+
+let a=Number(score)
+console.log(typeof a);
+
+let b=1
+bool = Boolean(b)
+console.log(bool);
+console.log(typeof bool);
+
+let c=75
+str=String(c)
+console.log(str);
+console.log(typeof str);
