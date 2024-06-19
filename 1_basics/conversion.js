a78ff98710ec2f8ed1c64dcfca60548937f46286
nishant-dotcom <prajapatinishant2433@gmail.com>
2024-06-19 04:08:17 +0000

    operations

--- 1_basics/conversion.js
+++ 1_basics/conversion.js
@@ -13,3 +13,18 @@ let c=75
 str=String(c)
 console.log(str);
 console.log(typeof str);
+
+// operations
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1= "hello"
+let str2= " Nishant"
+console.log(str1+str2);
+
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
